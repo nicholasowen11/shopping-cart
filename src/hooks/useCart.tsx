e9517d5fb2d28c0f8260957491aaea1ae3a2fb96
nicho.owen11 <161595086+nicholasowen11@users.noreply.github.com>
2025-02-17 16:14:14 +0700

    Update useCart.tsx

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -6,7 +6,6 @@ const useCart = (): UseCartContextType => {
     return useContext(CartContext)
 }
 
-=======
 import { useContext } from "react";
 import CartContext from "../context/CartProvider";
 import { UseCartContextType } from "../context/CartProvider";
